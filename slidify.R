c97d56fe7cfd47434fbbe3002e38afe8e143c6f5
Sebastian <sebastian.campbell@sydney.edu.au>
2013-07-31 14:08:40 +1000

    Initial slide framework

---
+++ slidify.R
@@ -0,0 +1,8 @@
+# library(devtools)
+# install_github("slidify", "ramnathv", "dev")
+# install_github("slidifyLibraries", "ramnathv", "dev")
+
+library(slidify)
+slidify("index.Rmd")
+
+file.show("index.html")
