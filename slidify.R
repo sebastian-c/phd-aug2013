10039f4be72a3305320190f99f8f74330288ae6f
Sebastian <sebastian.campbell@sydney.edu.au>
2013-08-15 17:54:10 +1000

    Added references

--- slidify.R
+++ slidify.R
@@ -1,6 +1,7 @@
 # library(devtools)
 # install_github("slidify", "ramnathv", "dev")
 # install_github("slidifyLibraries", "ramnathv", "dev")
+# install_github("knitcitations", "cboettig")
 
 library(slidify)
 slidify("index.Rmd")
